140b173e21df43f638c4671960b5be0ea5cc00b1
bagayado65 <58890509+bagayado65@users.noreply.github.com>
2021-02-01 13:20:51 +0700

    commit

---
+++ Register/register.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
+    <link rel="icon" href="../IMG/icons8-room-90.png" type="image/x-icon">
+    <title>Register BAMR</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
