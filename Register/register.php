7ac6f26a3ea2dfd12e1f76d29ac119cc2019746c
bagayado65 <58890509+bagayado65@users.noreply.github.com>
2021-02-01 14:14:25 +0700

    commit

--- Register/register.php
+++ Register/register.php
@@ -4,13 +4,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
     <link rel="icon" href="../IMG/icons8-room-90.png" type="image/x-icon">
+    <link rel="stylesheet" href="../CSS/Register.css">
+    <script src="https://cdn.jsdelivr.net/npm/vue@2.x/dist/vue.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.js"></script>
     <title>Register BAMR</title>
 </head>
 
 <body>
-
+<div class="mx-auto" id="app">
+{{ message }}
+</div>
+    <script>
+        var app = new Vue({
+            el: '#app',
+            vuetify: new Vuetify(),
+            data: {
+                message: 'Hello Vue!'
+            },
+        })
+    </script>
 </body>
 
 </html>
